9cdc0a06208e2efc63d8d5f216bf98913069a6a4
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-05 17:44:04 -0600

    Many failed attempts to update a users name. Still working on the Decorator to combine two names to meet a users params.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,21 +17,29 @@ class UsersController < ApplicationController
       render :login_form    
     end  
   end
-
+    
   def edit
     user_attr = UserFacade.get_by_id(params[:id])
     @user = UserDecorator.new(user_attr)
   end 
 
   def update  
-    require 'pry'; binding.pry
-    @user = UserFacade.get_by_id(params[:id])
-    @user.update(user_params)
+    user = UserFacade.get_by_id(params[:id])
+    new_name = params[:first_name] +" "+ params[:last_name] 
+    # require 'pry'; binding.pry
+    user.name = new_name
+
+    user.object.update(user_params)
+    if user.save 
+      redirect_to user_path(user)
+    else 
+      render :edit 
+    end 
   end
 
   private 
 
-  # def user_params
-  #   params.require(:user).permit(:name, :email, :password) 
-  # end
+  def user_params 
+    params.require(:user).permit(:name, :email, :password)
+  end
 end
