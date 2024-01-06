91ee5c5f4e46c963537d44b5408db12bb33a0b10
Miranda Delaney <delaney.miranda1@gmail.com>
2024-01-06 13:27:35 -0700

    Adds testing for user dog index

--- spec/features/users/dogs/index_spec.rb
+++ spec/features/users/dogs/index_spec.rb
@@ -1,6 +1,32 @@
 require "rails_helper"
 
 RSpec.describe "User Dog Index" do
+  before(:each) do 
+    @user1 = 
+  end
 
+  it 'displays dogs with their name, breed, size, fixed status and photo of dog' do
+    visit '/users/:id/dogs'
+    expect(page).to have_content("#{@user1.name}'s Dogs")
+    expect(page).to have_content("Name:")
+    expect(page).to have_content("Breed:")
+    expect(page).to have_content("Age:")
+    expect(page).to have_content("Size:")
+    expect(page).to have_content("Fixed:")
+    # research how to test for dog photo
+  end
+
+  xit 'has a button to edit dog and said button redirects to dog edit path' do
+    visit '/users/:id/dogs'
+    expect(page).to have_button("Edit Dog")
+    click_button "Edit Dog"
+    expect(current_path).to eq('/users/:id/dogs/:dog_id/edit')
+  end
   
+  xit 'has a button to add dog and said button redirects to dog new path' do
+    visit '/users/:id/dogs'
+    expect(page).to have_button("Add Dog")
+    click_button "Add Dog"
+    expect(current_path).to eq('/users/:id/dogs/new')
+  end
 end
