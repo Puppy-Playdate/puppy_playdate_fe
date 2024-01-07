786bd46e7908d0a3df6b700e24d59947d02f110b
Eric Belongea <eric.belongea@gmail.com>
2024-01-07 13:20:59 -0700

    Starts to set up google address validation

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -1,7 +1,14 @@
 class SocialsController < ApplicationController
   def index;end
 
-  def new;end
+  def new
+    # require 'pry'; binding.pry
+  end
+
+  def create
+    # You have to send an address object as a post request to the Google Address Validator API
+    require 'pry'; binding.pry
+  end
 
   def guest_index
     render :index
---
+++ spec/features/socials/new_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe 'Socials New' do
+  it 'exists' do
+    visit new_user_social_path(3)
+
+    
+    expect().to eq()
+  end
+end
