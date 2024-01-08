432824c125fd7625ccdd2d724aaa790502941118
Eric Belongea <eric.belongea@gmail.com>
2024-01-08 08:59:25 -0700

    Does refactoring in the GoogleFacade for better SRP

--- app/controllers/socials_controller.rb
+++ app/controllers/socials_controller.rb
@@ -6,9 +6,7 @@ class SocialsController < ApplicationController
   end
 
   def create
-    # address = { locality: params[:locality], addressLines: params[:addressLines]}
-    # social = {name: params[:name], date: params[:date], time: [params["[time(4i)]"], params["[time(5i)]"]]}
-    address_response = GoogleFacade.verify_address(params)
+    address_response = GoogleFacade.verify_address_and_create_social(params)
     require 'pry'; binding.pry
   end
 
--- app/facades/google_facade.rb
+++ app/facades/google_facade.rb
@@ -1,6 +1,28 @@
 class GoogleFacade
-  def self.verify_address(params)
-    address_object =  {
+  def self.verify_address_and_create_social(params)
+    address_object = build_address_object(params)
+
+    address_response = GoogleService.verify_address(address_object)
+    # Line below is only needed in order to build social object
+    response_body = JSON.parse(address_response.body, symbolize_names: true)
+
+    require 'pry'; binding.pry
+    # if address_response.status == 200
+    #   social_response = SocialsFacade.create_social(data)
+    #   if social_response.status == 201
+    #     flash[:success] = "Social Created"
+    #   else
+    #     flash[:error] = social_response[:error]
+    #   end
+    # else
+    #   flash[:error] = address_response[:error]
+    # end
+  end
+
+  private
+
+  def self.build_address_object(params)
+    {
       "address": {
         "regionCode": "US",
         "locality": params[:locality],
@@ -8,27 +30,14 @@ class GoogleFacade
       },
       "enableUspsCass": true
     }
-    
-    address_response = GoogleService.verify_address(address_object)
-    response_body = JSON.parse(address_response.body, symbolize_names: true)
+  end
 
-    social_data = {
+  def self.build_social_object(params, response_body)
+    {
       name: params[:name],
       date: params[:date],
-      time: social[:time].join(":"),
+      time:[params["[time(4i)]"], params["[time(5i)]"]].join(":"),
       location: response_body[:result][:address][:formattedAddress]
     }
-
-    require 'pry'; binding.pry
-    if address_response.status == 200
-      # social_response = SocialsFacade.create_social(data)
-      if social_response.status == 201
-        flash[:success] = "Social Created"
-      else
-        flash[:error] = social_response[:error]
-      end
-    else
-      flash[:error] = address_response[:error]
-    end
   end
 end
