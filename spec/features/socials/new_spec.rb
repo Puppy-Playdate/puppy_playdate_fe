6a31ef17583c6d3a8dae513227042aa244061001
Eric Belongea <eric.belongea@gmail.com>
2024-01-09 23:07:49 -0700

    Starts testing

--- spec/features/socials/new_spec.rb
+++ spec/features/socials/new_spec.rb
@@ -1,10 +1,19 @@
 require "rails_helper"
 
 RSpec.describe 'Socials New' do
-  xit 'exists' do
-    visit new_user_social_path(3)
+  before :each do
+    
+  end
 
+  it 'exists', :vcr do
+    visit user_path(3)
+    # save_and_open_page
+    expect(page).to have_button("Create a Social")
+    click_button("Create a Social")
 
-    expect().to eq()
+    expect(current_path).to eq( new_user_social_path(3))
+
+    expect(page).to have_content("Create a Social")
+    expect(page).to have_field(:name)
   end
 end
