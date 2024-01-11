889949e91c7d11f79dc9551ac298c4949bfea426
Blake Sergesketter <blakesergesketter@gmail.com>
2024-01-11 17:26:59 -0600

    TEST- UsersService create_user testing. All tests passing.

--- spec/services/users_service_spec.rb
+++ spec/services/users_service_spec.rb
@@ -41,7 +41,26 @@ RSpec.describe UsersService do
       expect(attributes[:email]).to eq("randy@gmail.com")
       expect(attributes).to have_key :dog_count
       expect(attributes[:dog_count]).to eq(0)
-
     end 
+
+    it '#create_user creates a new user', :vcr do
+      params = { name: "Karl P. Sherman", email: "up_dawg13@aol.com", password: "loki123" }
+      response = UsersService.create_user(
+        params[:name], params[:email], params[:password]
+      )
+      expect(response.status).to eq(201)
+
+      karl = JSON.parse(response.body, symbolize_names: true)
+      expect(karl).to be_a Hash
+
+      attributes = karl[:data][:attributes]
+
+      expect(attributes).to have_key :name
+      expect(attributes[:name]).to eq("Karl P. Sherman")
+      expect(attributes).to have_key :email
+      expect(attributes[:email]).to eq("up_dawg13@aol.com")
+      expect(attributes).to have_key :dog_count
+      expect(attributes[:dog_count]).to eq(0)
+    end   
   end 
 end 
